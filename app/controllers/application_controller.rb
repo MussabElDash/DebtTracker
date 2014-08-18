76f365d40434551c4d602014f1e9fb13e4e2e7c7
MussabElDash <mussab14@gmail.com>
2014-08-18 07:37:22 +0300

    sign in like noraml and by facebook and google - sign up - sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+	before_action :authenticate!
+	before_action :update_sanitized_params, if: :devise_controller?
+
+private
+	def update_sanitized_params
+		devise_parameter_sanitizer.for(:sign_up) {
+					|user| user.permit(:email, :user_name,
+						:first_name, :last_name, :image,
+						:password, :password_confirmation)
+				}
+				devise_parameter_sanitizer.for(:account_update) {
+					|user| user.permit(:email, :user_name, :current_password,
+						:first_name, :last_name, :image,
+						:password, :password_confirmation)
+				}
+	end
+
+	def authenticate!
+		unless user_signed_in? or params[:controller] == nil or params[:controller].include?"devise"
+			flash[:notice] = "You're not logged in!"
+			redirect_to :root
+		end
+	end
 end
